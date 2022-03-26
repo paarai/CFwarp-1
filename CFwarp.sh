d3b84f7960f0fed5181a8ffa4bc4fc26bfd75597
甬哥kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-03-26 16:05:50 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -174,8 +174,8 @@ docker(){
 if [[ -n $(ip a | grep docker) ]]; then
 red "检测到VPS已安装docker，如继续安装Wgcf-WARP，docker会失效"
 sleep 3s
-yellow "3秒后继续安装，退出安装请按Ctrl+c"
-sleep 3s
+yellow "6秒后继续安装，退出安装请按Ctrl+c"
+sleep 6s
 fi
 }
 
